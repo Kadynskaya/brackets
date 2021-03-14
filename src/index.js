aa3cceeaa581742c9c39f75f8fca10c60939ab9c
Kadynskaya <olgakad9@gmail.com>
2021-03-14 15:24:19 +0200

    init: brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+    let bracketsPairsObj = bracketsConfig.reduce((acc, [opening, closing]) => Object.assign(acc, { [opening]: closing }), {});
+
+    for(let bracket of str.split('')) {
+        if(bracket === bracketsPairsObj[bracket] && stack[stack.length - 1] === bracket) {
+            stack.pop();
+        } else if(bracketsPairsObj[bracket]){
+            stack.push(bracket);
+        } else if(bracket !== bracketsPairsObj[stack.pop()]) {
+            return false;
+        }
+    }
+
+  return !stack.length;
 }
