852433a74c6778882a670d801ece433819619b62
Kadynskaya <olgakad9@gmail.com>
2021-03-14 15:30:23 +0200

    fix: fixed indents

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
   let stack = [];
-    let bracketsPairsObj = bracketsConfig.reduce((acc, [opening, closing]) => Object.assign(acc, { [opening]: closing }), {});
+  let bracketsPairsObj = bracketsConfig.reduce((acc, [opening, closing]) => Object.assign(acc, { [opening]: closing }), {});
 
-    for(let bracket of str.split('')) {
-        if(bracket === bracketsPairsObj[bracket] && stack[stack.length - 1] === bracket) {
-            stack.pop();
-        } else if(bracketsPairsObj[bracket]){
-            stack.push(bracket);
-        } else if(bracket !== bracketsPairsObj[stack.pop()]) {
-            return false;
-        }
+  for(let bracket of str.split('')) {
+    if(bracket === bracketsPairsObj[bracket] && stack[stack.length - 1] === bracket) {
+      stack.pop();
+    } else if(bracketsPairsObj[bracket]){
+      stack.push(bracket);
+    } else if(bracket !== bracketsPairsObj[stack.pop()]) {
+      return false;
     }
+  }
 
   return !stack.length;
 }
